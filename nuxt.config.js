3960be71dca1f9377015eb8a21a0cd6999275556
san randry <sinrandry@gmail.com>
2021-01-09 19:58:13 +0300

    add push dir and chage base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,11 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
 export default theme({
   docs: {
-    primaryColor: '#E24F55'
-  }
-})
+    primaryColor: "#E24F55",
+  },
+  target: "static",
+  router: {
+    base: "/elementor_note/",
+  },
+});
